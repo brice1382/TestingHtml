a08ef9820d9eb96a3d74e3f5da8ff82c2fceb712
brice721 <brice721@hotmail.com>
2017-02-17 01:52:46 +0000

    moms site
    trying to get me some source control

---
+++ TestingHtml/TestingHtml/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingHtml.Startup))]
+namespace TestingHtml
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
